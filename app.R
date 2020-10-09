44fef0d6d7fef6e941f42d775462638c61a0eca6
Agasax <lars.saxhaug@gmail.com>
2020-10-09 20:20:31 +0200

    Added zoom

--- app.R
+++ app.R
@@ -1,34 +1,35 @@
 library(shiny)
 library(tidyverse)
 library(scales)
-# Define UI 
+# Define UI
 ui <- fluidPage(
   # Application title
   titlePanel("Leaf plot"),
   wellPanel(HTML(
     paste(
-      'This leaf plot displays the relationship between pre-test and post-test probability, given selected sensitivity and specificity  ',
-      'Upper curve shows post-test probabilities given positive test',
-      'Lower curve shows post-test probabilities given negative test',
-      'Dotted lines mark post-test probability given selected pre-test probability',
-      sep = '<br/>'
+      "This leaf plot displays the relationship between pre-test and post-test probability, given selected sensitivity and specificity  ",
+      "Upper curve shows post-test probabilities given positive test",
+      "Lower curve shows post-test probabilities given negative test",
+      "Dotted lines mark post-test probability given selected pre-test probability",
+      sep = "<br/>"
     )
-  ))
-  ,
+  )),
   # Sidebar with sliders and input boxes
   sidebarLayout(
-    sidebarPanel(
-      inputPanel(
-        
-        numericInput('sensitivity', value = 90, label = 'Sensitivity (%):', min=0, max=100, step = 1),
-        
-        numericInput('specificity', value = 55, label = 'Specificity (%):', min=0, max=100, step = 1),
-        
-        numericInput('preprob',  value = 15, label = 'Pretest probability (%):', min=0, max=100, step = 1)
-      )
-    ),
+    sidebarPanel(tabsetPanel(tabPanel(title = "Input",
     
-    # Show a plot and 
+        numericInput("sensitivity", value = 90, label = "Sensitivity (%):", min = 0, max = 100, step = .1),
+
+        numericInput("specificity", value = 55, label = "Specificity (%):", min = 0, max = 100, step = .1),
+
+        numericInput("preprob", value = 15, label = "Pretest probability (%):", min = 0, max = 100, step = .1)
+      
+    ),tabPanel(title = "Options",
+               sliderInput("xlim",value = c(0,100), label = "Range of pre-test probability in %", post = "%",min = 0,max = 100,step = .1),
+               sliderInput("ylim",value = c(0,100), label = "Range of post-test probability in %", post = "%",min = 0,max = 100,step = .1)
+              ))),
+
+    # Show a plot and
     mainPanel(plotOutput("leafPlot"))
   ),
   wellPanel(HTML(
@@ -37,20 +38,20 @@ ui <- fluidPage(
 Leaf plot v 0.9
 Script by Fernando Zampieri
 (fzampieri@hcor.com.br) from "When will less monitoring and diagnostic testing benefit the patient more?" FG Zampieri, S Einav. Intensive Care Med. 2019.',
-      
-      'Original concept by:
-Coulthard MG, Coulthard T. The leaf plot: a novel way of presenting the value of tests. Br J Gen Pract. 2019 Apr;69(681):205-206.',
-      'Made Shiny by Lars Mølgaard Saxhaug  (lars.molgaard.saxhaug@ntnu.no)',
-      'Built on R 3.6.2 using tidyverse version 1.3.0 and shiny version 1.4.0.2',
-      'Use and distribute freely!',
-      sep = '<br/>'
+
+      "Original concept by:
+Coulthard MG, Coulthard T. The leaf plot: a novel way of presenting the value of tests. Br J Gen Pract. 2019 Apr;69(681):205-206.",
+      "Made Shiny by Lars Mølgaard Saxhaug  (lars.molgaard.saxhaug@ntnu.no)",
+      "Built on R 3.6.2 using tidyverse version 1.3.0 and shiny version 1.4.0.2",
+      "Use and distribute freely!",
+      sep = "<br/>"
     )
   ))
 )
 
 
-#Leafplotfunction
-leafplot <- function(sens, spec, preprob) {
+# Leafplotfunction
+leafplot <- function(sens, spec, preprob, xlim, ylim) {
   db <- tibble(
     prob.pre = seq(0.001, 0.999, by = 0.001),
     odds.pre = prob.pre / (1 - prob.pre),
@@ -62,11 +63,11 @@ leafplot <- function(sens, spec, preprob) {
     prob.pos.n = odds.pos.n / (1 + odds.pos.n)
   )
   # Post.prob. given selected pre.prob
-  post.prob.p.print <-( preprob / (1 - preprob) * sens / (1 - spec) / (1 + preprob / (1 - preprob) * sens / (1 - spec)))
-  post.prob.n.print <-( preprob / (1 - preprob) * (1 - sens) / spec / (1 + preprob / (1 - preprob) * (1 - sens) / spec))
-  
-  #Plot
-  leafplot <-  db %>%
+  post.prob.p.print <- (preprob / (1 - preprob) * sens / (1 - spec) / (1 + preprob / (1 - preprob) * sens / (1 - spec)))
+  post.prob.n.print <- (preprob / (1 - preprob) * (1 - sens) / spec / (1 + preprob / (1 - preprob) * (1 - sens) / spec))
+
+  # Plot
+  leafplot <- db %>%
     select(prob.pre, prob.pos.p, prob.pos.n) %>%
     ggplot(aes(x = prob.pre, y = prob.pos.p)) +
     geom_line() +
@@ -89,7 +90,7 @@ leafplot <- function(sens, spec, preprob) {
         xend = preprob,
         yend = post.prob.p.print
       ),
-      arrow = arrow(length = unit(0.5, 'cm'), ends = 'last'),
+      arrow = arrow(length = unit(0.5, "cm"), ends = "last"),
       lty = "dotted"
     ) +
     geom_segment(
@@ -99,61 +100,69 @@ leafplot <- function(sens, spec, preprob) {
         xend = preprob,
         yend = post.prob.n.print
       ),
-      arrow = arrow(length = unit(0.5, 'cm'), ends = 'last'),
+      arrow = arrow(length = unit(0.5, "cm"), ends = "last"),
       lty = "dotted"
-    ) +       coord_cartesian(xlim = c(0, 1),
-                              expand = TRUE,
-                              ylim = c(0, 1)) +
-    scale_y_continuous(
+    ) +
+    coord_cartesian(
+      xlim = c(0, 1),
+      expand = TRUE,
+      ylim = c(0, 1)
+    ) +
+    scale_y_continuous(limits = ylim,
       labels = scales::percent,
       breaks = c(0, 0.25, 0.5, 0.75, 1, post.prob.p.print, post.prob.n.print)
     ) +
-    scale_x_continuous(labels = scales::percent,
-                       breaks = c(0, 0.25, 0.5, 0.75, 1, preprob)) +
+    scale_x_continuous( limits = xlim,
+      labels = scales::percent,
+      breaks = c(0, 0.25, 0.5, 0.75, 1, preprob)
+    ) +
     labs(
       x = "Pre-test probability",
       y = "Post-test probability",
       title = "",
       subtitle = paste(
-        'With a pre-test probability of',
+        "With a pre-test probability of",
         percent(preprob),
-        'post-test probability is',
+        "post-test probability is",
         percent(post.prob.p.print),
-        'given positive test and',
+        "given positive test and",
         percent(post.prob.n.print),
-        'given negative test',
-        sep = ' '
+        "given negative test",
+        sep = " "
       )
     ) +
     annotate("text",
-             x = 0.25,
-             y = 0.875,
-             label = "Positive Test") +
+      x = 0.25,
+      y = 0.875,
+      label = "Positive Test"
+    ) +
     annotate("text",
-             x = 0.875,
-             y = 0.25,
-             label = "Negative Test") +
-    theme_minimal()
-  
+      x = 0.875,
+      y = 0.25,
+      label = "Negative Test"
+    ) +
+  theme_minimal()
+
   return(list(db, leafplot, post.prob.p.print, post.prob.n.print))
 }
 
-# Define server 
+# Define server
 server <- function(input, output, session) {
-  #if Sensitivity
-  
+  # if Sensitivity
+
   output$leafPlot <-
     renderPlot({
-      leafplot(input$sensitivity / 100,
-               input$specificity / 100,
-               input$preprob / 100)
-      
+      leafplot(
+        input$sensitivity / 100,
+        input$specificity / 100,
+        input$preprob / 100,
+        input$xlim / 100,
+        input$ylim / 100
+      )
     })
-  
-  
 }
 
-#Tibble
+# Tibble
 
 
 
